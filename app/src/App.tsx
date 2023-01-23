f4c7834317bf8bd8dd8d783c933aba6782b9884d
Michael Huynh <mhuynh@twilio.com>
2023-01-23 14:35:36 -0800

    fix: rename entry points

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/app';
 import Login from './components/Login';
+import Home from './screens/Home';
 
+/**
+ * We want to import this as soon as possible in the application lifecycle to
+ * initialize our application state and bind to twilio-voice.
+ */
 import './util/voice';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Login />
+      <NavigationContainer>
+        <Stack.Screen name="Home" component={Home} />
+      </NavigationContainer>
     </Provider>
   );
 };
