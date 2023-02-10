48029a336641afc67b0d60a165cc8ce4764d1d28
Kevin <kchoy@twilio.com>
2023-02-09 16:04:12 -0800

    feat: auth0 login logout integration

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,7 @@ import {
   createBottomTabNavigator,
   type BottomTabNavigationOptions,
 } from '@react-navigation/bottom-tabs';
+import { useAuth0, Auth0Provider } from 'react-native-auth0';
 import React from 'react';
 import { Image } from 'react-native';
 import { Provider } from 'react-redux';
@@ -11,7 +12,9 @@ import { store } from './store/app';
 import Home from './screens/Home';
 import Dialer from './screens/Dialer';
 import Call from './screens/Call';
+import SignIn from './screens/SignIn';
 import { StackParamList, TabParamList } from './types';
+import config from './auth0-config';
 
 /**
  * We want to import this as soon as possible in the application lifecycle to
@@ -66,27 +69,50 @@ const Other: React.FC = () => {
   };
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Tab.Screen name="Home" component={Home} options={homeTabOptions} />
       <Tab.Screen name="Dialer" component={Dialer} options={dialerTabOptions} />
     </Tab.Navigator>
   );
 };
 
-const App = () => {
+const Auth0App: React.FC = () => {
+  const { user } = useAuth0();
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="App"
-            options={{ headerShown: false }}
-            component={Other}
-          />
-          <Stack.Screen name="Call" component={Call} />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          {user ? (
+            <>
+              <Stack.Screen
+                name="App"
+                options={{ headerShown: false }}
+                component={Other}
+              />
+              <Stack.Screen name="Call" component={Call} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="SignIn" component={SignIn} />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
+  );
+};
+
+const App = () => {
+  return (
+    <Auth0Provider domain={config.domain} clientId={config.clientId}>
+      <Auth0App />
+    </Auth0Provider>
   );
 };
 
--- app/src/screens/Home/index.tsx
+++ app/src/screens/Home/index.tsx
@@ -1,27 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const Home: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Ahoy!</Text>
-    </View>
-  );
-};
+import { useAuth0 } from 'react-native-auth0';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    backgroundColor: '#F4F4F6',
     height: '100%',
+    display: 'flex',
+    alignContent: 'center',
   },
-  title: {
-    fontSize: 48,
-    flexGrow: 1,
-    textAlignVertical: 'center',
-    color: 'black',
+  body: {
+    marginHorizontal: 40,
+    marginTop: 30,
+  },
+  client: {
+    marginTop: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  userText: {
+    fontWeight: '700',
+    fontSize: 24,
+    marginTop: 8,
+  },
+  logoContainer: {
+    marginTop: 100,
+    marginLeft: 18,
+  },
+  logoutText: {
+    color: '#0263E0',
+    textDecorationLine: 'underline',
+    padding: 0,
   },
 });
+
+const Home: React.FC = () => {
+  const { user, clearSession } = useAuth0();
+
+  const onLogout = async () => {
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../../../assets/icons/logo-twilio-red.png')}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.body}>
+        <Text>Ahoy!</Text>
+        <View style={styles.client}>
+          <Text>Client ID:</Text>
+          <TouchableHighlight onPress={onLogout}>
+            <Text style={styles.logoutText}>Log out</Text>
+          </TouchableHighlight>
+        </View>
+        <Text style={styles.userText}>{user?.email}</Text>
+      </View>
+    </View>
+  );
+};
 
 export default Home;
