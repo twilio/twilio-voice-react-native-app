74fba8abc8a6fd568a65daea16000d5ff3f524ff
Kevin <kchoy@twilio.com>
2023-02-28 16:28:43 -0800

    refactor: add process env for testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,11 @@ export function createExpressApp(twilioCredentials: TwilioCredentials) {
 
   const tokenRouter = Router();
   tokenRouter.use(createTokenRoute(twilioCredentials));
-  app.post('/token', jwtCheck, tokenRouter);
+  if (process.env.NODE_ENV === 'test') {
+    app.post('/token', tokenRouter);
+  } else {
+    app.post('/token', jwtCheck, tokenRouter);
+  }
 
   const twimlRouter = Router();
   twimlRouter.use(createTwimlRoute(twilioCredentials));
