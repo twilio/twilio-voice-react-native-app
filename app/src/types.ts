ace7b3c542250594436e9acdd320a2e89141c18f
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2023-03-07 15:01:56 -0800

    [VBLOCKS-1486] Fix recipient type. (#21)
    * fix(js): recipient type
    
    * fix(app): remove remaining references to pstn

--- app/src/types.ts
+++ app/src/types.ts
@@ -30,5 +30,3 @@ export type StackNavigationProp<T extends keyof StackParamList> =
     NativeStackNavigationProp<StackParamList, T>,
     BottomTabNavigationProp<TabParamList>
   >;
-
-export type RecipientType = 'client' | 'pstn';
