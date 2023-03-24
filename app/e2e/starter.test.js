ea83ac5951ab16cde27a0d1deca75a9a6471b140
Michael Huynh <mhuynh@twilio.com>
2023-03-24 12:36:54 -0700

    feat(app): android detox configs

---
+++ app/e2e/starter.test.js
@@ -0,0 +1,11 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    // await device.reloadReactNative();
+  });
+
+  it('should do nothing?', async () => {});
+});
