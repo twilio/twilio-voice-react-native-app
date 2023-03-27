ad6351e268443210a71ed96244d89b09b749ffcd
Michael Huynh <mhuynh@twilio.com>
2023-03-27 09:22:47 -0700

    fix(app): avd name

--- app/.detoxrc.js
+++ app/.detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: '.*'
+        avdName: 'TwilioVoiceReactNativeReferenceApp_AVD'
       }
     }
   },
