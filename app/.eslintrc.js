5f68194f42cc0b03c5a8d7cdb2da3cf90c6a7769
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2023-04-05 10:46:16 -0700

    fix(app): dialer hooks refactor (#44)
    * fix(app): dialer hooks refactor
    
    * fix(app): eslint error on shadow
    
    * fix(app): revert eslint no-shadow changes

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,13 +3,16 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ['coverage/'],
+  globals: {
+    globalThis: false, // false denotes not-writable
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
-        'no-undef': 'off',
       },
     },
   ],
