15c769b29121553562b0edbad9d2c03443c15df7
Kevin <kchoy@twilio.com>
2023-04-06 16:37:52 -0700

    feat: server logging

--- server/src/middlewares/log.ts
+++ server/src/middlewares/log.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { log } from '../utils/log';
 
 function logMiddleware(req: Request, _res: Response, next: NextFunction): void {
-  log(Date().toLocaleString(), req.method, req.originalUrl);
+  log(`${req.method} ${req.originalUrl}`);
 
   next();
 }
