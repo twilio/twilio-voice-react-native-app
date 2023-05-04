0e554291dbf1818256edf4de31dd4465bddce6a9
Michael Huynh <mhuynh@twilio.com>
2023-05-04 14:52:55 -0700

    fix(app): include new jest expectations

--- app/jest.config.js
+++ app/jest.config.js
@@ -2,6 +2,7 @@
 const config = {
   moduleFileExtensions: ['js', 'ts', 'tsx'],
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
   collectCoverage: true,
 };
