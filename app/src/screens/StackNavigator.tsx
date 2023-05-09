dcc53545b0cabc90b115e0badf8d803c23689d71
Michael Huynh <mhuynh@twilio.com>
2023-05-09 12:44:59 -0700

    fix(app): rename loading to busy

--- app/src/screens/StackNavigator.tsx
+++ app/src/screens/StackNavigator.tsx
@@ -8,7 +8,7 @@ import { type StackParamList } from './types';
 import SignIn from './SignIn';
 import { type State } from '../store/app';
 import CallInvite from './CallInvite';
-import Loading from '../components/Loading';
+import Busy from '../components/Busy';
 
 const Stack = createNativeStackNavigator<StackParamList>();
 
@@ -20,7 +20,7 @@ const StackNavigator = () => {
   }
 
   if (user?.status === 'pending') {
-    return <Loading />;
+    return <Busy />;
   }
 
   const screens =
