aeb1f1b53e404b1b2864a1bf259237f021632430
Kevin <kchoy@twilio.com>
2023-06-30 14:04:15 -0700

    refactor: audioDevices; add createTypedAsyncThunk

--- app/src/store/__tests__/audioDevices.test.ts
+++ app/src/store/__tests__/audioDevices.test.ts
@@ -1,6 +1,7 @@
 import * as audioDevices from '../voice/audioDevices';
-import * as app from '../app';
+import { createStore, Store } from '../app';
 import { voice } from '../../util/voice';
+import { miniSerializeError } from '@reduxjs/toolkit';
 
 let MockCall: { Event: Record<string, string> };
 
@@ -11,7 +12,7 @@ jest.mock('../../../src/util/fetch', () => ({
 jest.mock('../../../src/util/voice', () => ({
   voice: {
     connect: jest.fn(),
-    getAudioDevices: jest.fn().mockReturnValue({
+    getAudioDevices: jest.fn().mockResolvedValue({
       audioDevices: [
         { uuid: '1111', type: 'speaker', name: 'device1' },
         { uuid: '2222', type: 'bluetooth', name: 'device2' },
@@ -42,10 +43,10 @@ jest.mock('@twilio/voice-react-native-sdk', () => {
 });
 
 describe('audioDevices store', () => {
-  let store: app.Store;
+  let store: Store;
 
   beforeEach(() => {
-    store = app.createStore();
+    store = createStore();
   });
 
   it('gets an audio device', async () => {
@@ -53,12 +54,14 @@ describe('audioDevices store', () => {
     expect(store.getState().voice.audioDevices?.status).toEqual('fulfilled');
   });
 
-  it('handles audio device error', async () => {
-    jest
-      .spyOn(voice, 'getAudioDevices')
-      .mockRejectedValue(new Error('audio device error'));
+  it('handles audio device fetch error', async () => {
+    jest.spyOn(voice, 'getAudioDevices').mockRejectedValue('foo');
     await store.dispatch(audioDevices.getAudioDevices());
-    expect(store.getState().voice.audioDevices?.status).toEqual('rejected');
+    expect(store.getState().voice.audioDevices).toEqual({
+      reason: 'GET_AUDIO_DEVICES_ERROR',
+      status: 'rejected',
+      error: miniSerializeError('foo'),
+    });
   });
 
   it('handles no selected device', async () => {
@@ -68,7 +71,7 @@ describe('audioDevices store', () => {
       { uuid: '3333', type: 'earpiece', name: 'device3' },
     ];
     jest.spyOn(voice, 'getAudioDevices').mockImplementation(
-      jest.fn().mockReturnValue({
+      jest.fn().mockResolvedValue({
         audioDevices: audioDevicesMap,
       }),
     );
@@ -77,6 +80,37 @@ describe('audioDevices store', () => {
       status: 'fulfilled',
       selectedDevice: null,
       audioDevices: audioDevicesMap,
+    });
+  });
+
+  it('handles audio device undefined', async () => {
+    jest.spyOn(voice, 'getAudioDevices').mockImplementation(
+      jest.fn().mockResolvedValue({
+        audioDevices: undefined,
+      }),
+    );
+    await store.dispatch(audioDevices.getAudioDevices());
+    expect(store.getState().voice.audioDevices).toEqual({
+      status: 'rejected',
+      reason: 'AUDIO_DEVICES_NOT_FOUND',
+    });
+  });
+
+  it('handles uuid undefined', async () => {
+    const audioDevicesMap = [
+      { uuid: '1111', type: 'speaker', name: 'device1' },
+      { uuid: undefined, type: 'bluetooth', name: 'device2' },
+      { uuid: '3333', type: 'earpiece', name: 'device3' },
+    ];
+    jest.spyOn(voice, 'getAudioDevices').mockImplementation(
+      jest.fn().mockResolvedValue({
+        audioDevices: audioDevicesMap,
+      }),
+    );
+    await store.dispatch(audioDevices.getAudioDevices());
+    expect(store.getState().voice.audioDevices).toEqual({
+      status: 'rejected',
+      reason: 'MISSING_AUDIO_DEVICE_UUID',
     });
   });
 });
