c7aac10c471bcc92a336898b8ade092f92471243
Kevin <kchoy@twilio.com>
2023-07-24 11:09:22 -0700

    refactor: remove mock-store from signin

--- app/src/screens/SignIn/__tests__/SignIn.test.tsx
+++ app/src/screens/SignIn/__tests__/SignIn.test.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import SignIn from '..';
 import { createStore } from '../../../store/app';
-import configureStore from 'redux-mock-store';
+import * as auth0 from '../../../../__mocks__/react-native-auth0';
+import * as user from '../../../store/user';
+import * as token from '../../../store/voice/accessToken';
+
+let fetchMock: jest.Mock;
+
+jest.mock('../../../../src/util/fetch', () => ({
+  fetch: (fetchMock = jest.fn()),
+}));
 
 describe('<SignIn />', () => {
   let store: ReturnType<typeof createStore>;
@@ -29,22 +37,20 @@ describe('<SignIn />', () => {
     ).toBeOnTheScreen();
   });
 
-  it('should show the accessToken error message', () => {
+  it('should show the accessToken error message', async () => {
     const errorMessage = 'wrong email';
-    const initialState = {
-      voice: {
-        accessToken: {
-          error: { message: errorMessage },
-          reason: 'TOKEN_RESPONSE_NOT_OK',
-          status: 'rejected',
-          statusCode: 401,
-        },
-      },
-    };
-    const mockStore = configureStore();
-    wrapper = ({ children }) => (
-      <Provider store={mockStore(initialState)}>{children}</Provider>
-    );
+    jest.spyOn(auth0, 'authorize').mockResolvedValueOnce({
+      accessToken: 'test token',
+      idToken: 'test id token',
+    });
+    const error = new Error(errorMessage);
+    fetchMock.mockResolvedValueOnce({
+      ok: false,
+      text: jest.fn().mockResolvedValueOnce(error),
+      status: 401,
+    });
+    await store.dispatch(user.login());
+    await store.dispatch(token.getAccessToken());
     render(<SignIn />, { wrapper });
     expect(screen.getByText(errorMessage)).toBeOnTheScreen();
   });
