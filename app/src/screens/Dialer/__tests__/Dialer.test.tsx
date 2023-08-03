8613c14a74926cd845d0f3029844438090e93e0f
mmalavalli <mmalavalli@twilio.com>
2023-08-02 17:41:39 -0700

    VBLOCKS-1659 UI tests for calling a phone number.

--- app/src/screens/Dialer/__tests__/Dialer.test.tsx
+++ app/src/screens/Dialer/__tests__/Dialer.test.tsx
@@ -25,50 +25,126 @@ describe('<Dialer />', () => {
     );
   });
 
-  it('should display the dialpad buttons', () => {
-    render(<Dialer />, { wrapper });
-    expect(screen.getByText('1')).toBeOnTheScreen();
-    expect(screen.getByText('2')).toBeOnTheScreen();
-    expect(screen.getByText('3')).toBeOnTheScreen();
-    expect(screen.getByText('4')).toBeOnTheScreen();
-    expect(screen.getByText('5')).toBeOnTheScreen();
-    expect(screen.getByText('6')).toBeOnTheScreen();
-    expect(screen.getByText('7')).toBeOnTheScreen();
-    expect(screen.getByText('8')).toBeOnTheScreen();
-    expect(screen.getByText('9')).toBeOnTheScreen();
-    expect(screen.getByText('*')).toBeOnTheScreen();
-    expect(screen.getByText('0')).toBeOnTheScreen();
-    expect(screen.getByText('#')).toBeOnTheScreen();
+  describe('call a phone number', () => {
+    beforeEach(() => {
+      render(<Dialer />, { wrapper });
+    });
+
+    describe('UI layout', () => {
+      it('should display the enabled dialpad buttons', () => {
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'].forEach(
+          (digit) => {
+            expect(screen.getByText(digit)).toBeOnTheScreen();
+          },
+        );
+      });
+
+      it('should display a disabled call button', () => {
+        const callButton = screen.getByTestId('call_button');
+        expect(callButton).toBeOnTheScreen();
+        expect(callButton.props.accessibilityState.disabled).toBe(true);
+      });
+
+      it('should display an enabled client button', () => {
+        const clientButton = screen.getByTestId('client_button');
+        expect(clientButton).toBeOnTheScreen();
+        expect(clientButton.props.accessibilityState.disabled).toBe(false);
+      });
+
+      it('should hide the backspace button', () => {
+        expect(screen.queryByTestId('back_space_button')).toBeFalsy();
+      });
+    });
+
+    describe('press the first dialpad button', () => {
+      beforeEach(() => {
+        fireEvent.press(screen.getByText('4'));
+      });
+
+      it('should display "+" followed by the digit of the dialpad button', () => {
+        const display = screen.getByTestId('formatted_number');
+        expect(display.props.children).toBe('+4');
+      });
+
+      it('should enable the call button', () => {
+        const callButton = screen.getByTestId('call_button');
+        expect(callButton.props.accessibilityState.disabled).toBe(false);
+      });
+
+      it('should show the backspace button', () => {
+        expect(screen.getByTestId('back_space_button')).toBeOnTheScreen();
+      });
+
+      describe('press the backspace button', () => {
+        beforeEach(() => {
+          fireEvent.press(screen.getByTestId('back_space_button'));
+        });
+
+        it('should clear the display', () => {
+          const display = screen.getByTestId('formatted_number');
+          expect(display.props.children).toBe('');
+        });
+
+        it('should hide the backspace button', () => {
+          expect(screen.queryByTestId('back_space_button')).toBeFalsy();
+        });
+
+        it('should disable the call button', () => {
+          const callButton = screen.getByTestId('call_button');
+          expect(callButton.props.accessibilityState.disabled).toBe(true);
+        });
+      });
+    });
+
+    describe('press more dialpad buttons', () => {
+      beforeEach(() => {
+        ['4', '1', '5', '6', '5', '0', '2', '8', '9', '0'].forEach((digit) => {
+          fireEvent.press(screen.getByText(digit));
+        });
+      });
+
+      it('should append the pressed dialpad digits to the display', () => {
+        const display = screen.getByTestId('formatted_number');
+        expect(display.props.children).toBe('+4156502890');
+      });
+
+      describe('press the backspace button', () => {
+        it('should remove the most recent digit added to the display', () => {
+          fireEvent.press(screen.getByTestId('back_space_button'));
+          const display = screen.getByTestId('formatted_number');
+          expect(display.props.children).toBe('+415650289');
+        });
+      });
+    });
+
+    describe('press the call button', () => {
+      beforeEach(() => {
+        ['4', '1', '5', '6', '5', '0', '2', '8', '9', '0'].forEach((digit) => {
+          fireEvent.press(screen.getByText(digit));
+        });
+        fireEvent.press(screen.getByTestId('call_button'));
+      });
+
+      it('should dispatch the "makeOutgoingCall" action', () => {
+        // TODO(mmalavalli)
+      });
+
+      it('should transition to the "ActiveCallScreen" component', () => {
+        // TODO(mmalavalli)
+      });
+    });
   });
 
-  it('should allow user to enter a phone number', () => {
-    render(<Dialer />, { wrapper });
-    fireEvent.press(screen.getByText('4'));
-    fireEvent.press(screen.getByText('1'));
-    fireEvent.press(screen.getByText('5'));
-    fireEvent.press(screen.getByText('7'));
-    fireEvent.press(screen.getByText('2'));
-    fireEvent.press(screen.getByText('4'));
-    fireEvent.press(screen.getByText('5'));
-    fireEvent.press(screen.getByText('6'));
-    fireEvent.press(screen.getByText('3'));
-    fireEvent.press(screen.getByText('8'));
-    expect(screen.getByText('+4157245638')).toBeOnTheScreen();
-  });
+  describe('call a client number', () => {
+    beforeEach(() => {
+      render(<Dialer />, { wrapper });
+      fireEvent.press(screen.getByTestId('client_button'));
+    });
 
-  it('should allow user to use the backspace button', () => {
-    render(<Dialer />, { wrapper });
-    fireEvent.press(screen.getByText('4'));
-    fireEvent.press(screen.getByText('1'));
-    fireEvent.press(screen.getByText('5'));
-    expect(screen.getByText('+415')).toBeOnTheScreen();
-    fireEvent.press(screen.getByTestId('back_space_button'));
-    expect(screen.getByText('+41')).toBeOnTheScreen();
-  });
-
-  it('should allow user to switch to client input', () => {
-    render(<Dialer />, { wrapper });
-    fireEvent.press(screen.getByText('Client'));
-    expect(screen.getByText('Number')).toBeOnTheScreen();
+    describe('UI layout', () => {
+      it('should pass', () => {
+        // TODO(mmalavalli)
+      });
+    });
   });
 });
