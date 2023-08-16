30f513e8667b92e4335bff4162931b40200e6934
Kevin <kchoy@twilio.com>
2023-08-16 15:34:33 -0700

    upgrade: auth0 3.x.x

--- app/src/util/auth.ts
+++ app/src/util/auth.ts
@@ -1,5 +1,5 @@
 import { getEnvVariable } from './env';
-import Auth0, { type SaveCredentialsParams } from 'react-native-auth0';
+import Auth0, { type Credentials } from 'react-native-auth0';
 
 export function initializeAuth() {
   const domain = getEnvVariable('DOMAIN_NAME');
@@ -12,27 +12,34 @@ export function initializeAuth() {
     clientId,
   });
 
-  const checkLoginStatus = async () => {
+  const checkLoginStatus = async (): Promise<{
+    accessToken: string;
+    email: string;
+  }> => {
     const { accessToken } = await auth0.credentialsManager.getCredentials();
     const { email } = await auth0.auth.userInfo({ token: accessToken });
+    if (typeof email === 'undefined') {
+      throw new Error('EMAIL_UNDEFINED');
+    }
     return {
       accessToken,
       email,
     };
   };
 
-  const login = async () => {
+  const login = async (): Promise<{ accessToken: string; email: string }> => {
     const credentials = await auth0.webAuth.authorize({ audience, scope });
     if (!credentials.idToken) {
       throw new Error('ID_TOKEN_UNDEFINED');
     }
     const { accessToken } = credentials;
-    await auth0.credentialsManager.saveCredentials(
-      credentials as SaveCredentialsParams,
-    );
+    await auth0.credentialsManager.saveCredentials(credentials as Credentials);
     const { email } = await auth0.auth.userInfo({
       token: credentials.accessToken,
     });
+    if (typeof email === 'undefined') {
+      throw new Error('EMAIL_UNDEFINED');
+    }
     return {
       accessToken,
       email,
