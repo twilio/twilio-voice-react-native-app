359a2749825cca7e6f1d225ce94aa50d9c01c221
Michael Huynh <mhuynh@twilio.com>
2023-08-21 18:18:25 -0700

    feat: consume notification tapped event

--- app/src/store/bootstrap.ts
+++ app/src/store/bootstrap.ts
@@ -104,6 +104,8 @@ export const bootstrapCallInvites = createTypedAsyncThunk<
 >(
   bootstrapCallInvitesActionTypes.prefix,
   async (_, { dispatch, getState, rejectWithValue }) => {
+    const navigate = await getNavigate();
+
     /**
      * Handle an incoming, pending, call invite.
      */
@@ -141,10 +143,6 @@ export const bootstrapCallInvites = createTypedAsyncThunk<
         dispatch(handleCall({ call }));
         handleSettledCallInvite(callInvite);
 
-        const navigate = getNavigate();
-        if (!navigate) {
-          return;
-        }
         const callSid = callInvite.getCallSid();
         navigate('Call', { callSid });
       },
@@ -226,13 +224,18 @@ export const bootstrapNavigationActionTypes = generateThunkActionTypes(
 );
 export const bootstrapNavigation = createTypedAsyncThunk(
   bootstrapNavigationActionTypes.prefix,
-  (_, { getState }) => {
+  async (_, { getState }) => {
+    const navigate = await getNavigate();
+
     const state = getState();
 
-    const navigate = getNavigate();
-    if (typeof navigate === 'undefined') {
-      return;
-    }
+    /**
+     * If the call invite notification body is tapped, navigate to the call
+     * invite screen.
+     */
+    voice.on(Voice.Event.CallInviteNotificationTapped, () => {
+      navigate('Incoming Call');
+    });
 
     if (state.voice.call.activeCall.ids.length) {
       navigate('Call', {});
