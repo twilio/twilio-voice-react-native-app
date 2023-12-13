7fd8f2b4390b4f8587b87a02ba5190d733d477d7
Ardavon Falls <afalls@Ardavons-MacBook-Pro-2.local>
2023-12-12 23:59:32 -0800

    force test case to approve required permissions

--- app/android/app/src/androidTest/java/com/twiliovoicereactnativereferenceapp/DetoxTest.java
+++ app/android/app/src/androidTest/java/com/twiliovoicereactnativereferenceapp/DetoxTest.java
@@ -1,5 +1,8 @@
 package com.twiliovoicereactnativereferenceapp;
 
+import android.Manifest;
+import android.os.Build;
+
 import com.wix.detox.Detox;
 import com.wix.detox.config.DetoxConfig;
 
@@ -10,12 +13,16 @@ import org.junit.runner.RunWith;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class DetoxTest {
+    private static final String[] permissionList;
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, false, false);
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(permissionList);
 
     @Test
     public void runDetoxTests() {
@@ -25,5 +32,19 @@ public class DetoxTest {
         detoxConfig.rnContextLoadTimeoutSec = 180;
 
         Detox.runTests(mActivityRule, detoxConfig);
+    }
+
+    static {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) {
+            permissionList = new String[] {
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.POST_NOTIFICATIONS };
+        } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
+            permissionList = new String[] {
+                    Manifest.permission.RECORD_AUDIO, Manifest.permission.BLUETOOTH_CONNECT };
+        } else {
+            permissionList = new String[] { Manifest.permission.RECORD_AUDIO };
+        }
     }
 }
