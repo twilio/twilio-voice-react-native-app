fc23d2eec03a4afb50e050cb764d8546288563d5
Michael Huynh <mhuynh@twilio.com>
2024-10-01 12:22:40 -0700

    fix: add back configs

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@react-navigation|@react-native|react-native)'
+  ],
+  collectCoverage: true,
 };
+
+module.exports = config;
--- app/metro.config.js
+++ app/metro.config.js
@@ -1,8 +1,26 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const defaultSourceExts =
+  require('metro-config/src/defaults/defaults').sourceExts;
+
 /**
  * Metro configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    sourceExts:
+      process.env.MY_APP_MODE === 'mocked'
+        ? ['mock.ts', ...defaultSourceExts]
+        : defaultSourceExts,
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
